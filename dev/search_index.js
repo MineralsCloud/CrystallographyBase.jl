55e72b67c1a2c471b74cc81b780eaf99713989b9
Documenter.jl <documenter@juliadocs.github.io>
2021-06-08 07:37:33 +0000

    build based on 13c8d20

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CrystallographyBase","category":"page"},{"location":"#CrystallographyBase","page":"Home","title":"CrystallographyBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CrystallographyBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CrystallographyBase]","category":"page"}]
+}
