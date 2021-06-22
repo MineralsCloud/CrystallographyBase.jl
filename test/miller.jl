d307aef208490cbea42cfddb8cda75ea794a7f16
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-22 16:09:45 -0400

    Fix tests

--- test/miller.jl
+++ test/miller.jl
@@ -1,14 +1,3 @@
-@testset "Test constructors" begin
-    @test_throws TypeError Miller{Int,Int}
-    @test_throws TypeError MillerBravais{Int,Int}
-    @test_throws TypeError Miller{Int}(1, 2, 3)
-    @test_throws TypeError MillerBravais{Int}(1, 2, 3, 4)
-    @test_throws TypeError Miller{Int}([1, 2, 3])
-    @test_throws TypeError MillerBravais{Int}([1, 2, 3, 4])
-    @test_throws TypeError Miller{Int}((1, 2, 3))
-    @test_throws TypeError MillerBravais{Int}((1, 2, 3, 4))
-end
-
 @testset "Test conversion between real `Miller` and `MillerBravais`" begin
     millerreal = [[1, 1, 1], [-1, 0, 1], [0, -1, 1], [-1, -1, 1], [1, 0, 1], [0, 1, 1]]
     millerbravaisreal = [
@@ -20,18 +9,16 @@ end
         [-1, 2, -1, 3],
     ]
     for (x, y) in zip(millerreal, millerbravaisreal)
-        m = Miller{RealSpace}(x)
-        mb = MillerBravais{RealSpace}(y)
-        @test convert(MillerBravais{RealSpace}, m) == mb
-        @test convert(Miller{RealSpace}, mb) == m
-        @test convert(Miller{RealSpace}, m) == m
-        @test convert(MillerBravais{RealSpace}, mb) == mb
-        @test_throws MethodError convert(Miller{ReciprocalSpace}, m)
-        @test_throws MethodError convert(MillerBravais{ReciprocalSpace}, mb)
-        @test_throws MethodError convert(MillerBravais{ReciprocalSpace}, m)
-        @test_throws MethodError convert(Miller{ReciprocalSpace}, mb)
-        @test_throws MethodError convert(MillerBravais, m)
-        @test_throws MethodError convert(Miller, mb)
+        m = Miller(x)
+        mb = MillerBravais(y)
+        @test convert(MillerBravais, m) == mb
+        @test convert(Miller, mb) == m
+        @test convert(Miller, m) == m
+        @test convert(MillerBravais, mb) == mb
+        @test_throws MethodError convert(ReciprocalMiller, m)
+        @test_throws MethodError convert(ReciprocalMillerBravais, mb)
+        @test_throws MethodError convert(ReciprocalMillerBravais, m)
+        @test_throws MethodError convert(ReciprocalMiller, mb)
     end
 end
 
@@ -47,16 +34,14 @@ end
         [-1, 1, 0, 0],
     ]
     for (x, y) in zip(millerreciprocal, millerbravaisreciprocal)
-        m = Miller{ReciprocalSpace}(x)
-        mb = MillerBravais{ReciprocalSpace}(y)
-        @test convert(MillerBravais{ReciprocalSpace}, m) == mb
-        @test convert(Miller{ReciprocalSpace}, mb) == m
-        @test convert(Miller{ReciprocalSpace}, m) == m
-        @test convert(MillerBravais{ReciprocalSpace}, mb) == mb
-        @test_throws MethodError convert(Miller{RealSpace}, m)
-        @test_throws MethodError convert(MillerBravais{RealSpace}, mb)
-        @test_throws MethodError convert(MillerBravais{RealSpace}, m)
-        @test_throws MethodError convert(Miller{RealSpace}, mb)
+        m = ReciprocalMiller(x)
+        mb = ReciprocalMillerBravais(y)
+        @test convert(ReciprocalMillerBravais, m) == mb
+        @test convert(ReciprocalMiller, mb) == m
+        @test convert(ReciprocalMiller, m) == m
+        @test convert(ReciprocalMillerBravais, mb) == mb
+        @test_throws MethodError convert(Miller, m)
+        @test_throws MethodError convert(MillerBravais, mb)
         @test_throws MethodError convert(MillerBravais, m)
         @test_throws MethodError convert(Miller, mb)
     end
