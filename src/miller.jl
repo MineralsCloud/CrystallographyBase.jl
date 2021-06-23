f438d537d068d755508090242e23bdf28e12ab9c
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-23 02:07:25 -0400

    Add examples to `@m_str`

--- src/miller.jl
+++ src/miller.jl
@@ -86,6 +86,35 @@ end
     m_str(s)
 
 Generate the Miller indices or Miller–Bravais indices quickly.
+
+# Examples
+```jldoctest
+julia> m"[-1, 0, 1]"
+3-element Miller:
+ -1
+  0
+  1
+
+julia> m"<2, -1, -1, 3>"
+4-element MillerBravais:
+  2
+ -1
+ -1
+  3
+
+julia> m"(-1, 0, 1)"
+3-element ReciprocalMiller:
+ -1
+  0
+  1
+
+julia> m"(1, 0, -1, 0)"
+4-element ReciprocalMillerBravais:
+  1
+  0
+ -1
+  0
+```
 """
 macro m_str(s)
     r =
