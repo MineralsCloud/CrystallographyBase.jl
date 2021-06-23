MineralsCloud/CrystallographyBase.jl

commit 8361e72bde8b237c269c0a3cc2dbed868adc13af

Fix `Miller` & `MillerBravais`, add `ReciprocalMiller` & `ReciprocalMillerBravais`

src/miller.jl
@@ -1,43 +1,52 @@
-export Miller, MillerBravais, RealSpace, ReciprocalSpace
+export Miller, MillerBravais, ReciprocalMiller, ReciprocalMillerBravais
 export @m_str, @mb_str
 
-abstract type AbstractSpace end
-struct RealSpace <: AbstractSpace end
-struct ReciprocalSpace <: AbstractSpace end
-
-struct Miller{S<:AbstractSpace} <: AbstractVector{Int}
+abstract type AbstractMiller <: AbstractVector{Int} end
+struct Miller <: AbstractMiller
     data::SVector{3,Int}
-    Miller{S}(v) where {S} = new(iszero(v) ? v : v .÷ gcd(v))
+    Miller(v) = new(iszero(v) ? v : v .÷ gcd(v))
 end
-Miller{S}(i, j, k) where {S} = Miller{S}([i, j, k])
+Miller(i, j, k) = Miller([i, j, k])
+struct ReciprocalMiller <: AbstractMiller
+    data::SVector{3,Int}
+    ReciprocalMiller(v) = new(iszero(v) ? v : v .÷ gcd(v))
+end
+ReciprocalMiller(i, j, k) = ReciprocalMiller([i, j, k])
 
-struct MillerBravais{S<:AbstractSpace} <: AbstractVector{Int}
+abstract type AbstractMillerBravais <: AbstractVector{Int} end
+struct MillerBravais <: AbstractMillerBravais
     data::SVector{4,Int}
-    function MillerBravais{S}(v) where {S}
-        @assert(
-            v[3] == -v[1] - v[2],
-            "the 3rd index of `MillerBravais` should equal to the negation of the first two!"
-        )
+    function MillerBravais(v)
+        @assert v[3] == -v[1] - v[2] "the 3rd index of `MillerBravais` should equal to the negation of the first two!"
         return new(iszero(v) ? v : v .÷ gcd(v))
     end
 end
-MillerBravais{S}(i, j, k, l) where {S} = MillerBravais{S}([i, j, k, l])
-
-# This is a helper type and should not be exported!
-const INDICES = Union{Miller,MillerBravais}
+MillerBravais(i, j, k, l) = MillerBravais([i, j, k, l])
+struct ReciprocalMillerBravais <: AbstractMillerBravais
+    data::SVector{4,Int}
+    function ReciprocalMillerBravais(v)
+        @assert v[3] == -v[1] - v[2] "the 3rd index of `MillerBravais` should equal to the negation of the first two!"
+        return new(iszero(v) ? v : v .÷ gcd(v))
+    end
+end
+ReciprocalMillerBravais(i, j, k, l) = ReciprocalMillerBravais([i, j, k, l])
 
 # This is a helper function and should not be exported!
-function _indices_str(r::Regex, s::AbstractString, ::Type{T}) where {T<:INDICES}
+function _indices_str(r::Regex, s::AbstractString)
     m = match(r, strip(s))
     if m === nothing
         error("not a valid expression!")
     else
         brackets = first(m.captures) * last(m.captures)
         x = (parse(Int, x) for x in m.captures[2:(end-1)])
-        if brackets ∈ ("()", "{}")
-            return T{ReciprocalSpace}(x...)
-        elseif brackets ∈ ("[]", "<>")
-            return T{RealSpace}(x...)
+        if brackets == "()"
+            ReciprocalMiller(x...)
+        elseif brackets == "{}"
+            ReciprocalMillerBravais(x...)
+        elseif brackets == "[]"
+            Miller(x...)
+        elseif brackets == "<>"
+            MillerBravais(x...)
         else
             error("not a valid expression!")
         end
@@ -46,13 +55,13 @@ end
 
 macro m_str(s)
     r = r"([({[<])\s*([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]*([>\]})])"
-    _indices_str(r, s, Miller)
+    return _indices_str(r, s)
 end
 
 macro mb_str(s)
     r =
         r"([({[<])\s*([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]*([>\]})])"
-    _indices_str(r, s, MillerBravais)
+    return _indices_str(r, s)
 end
 
 Base.size(::Miller) = (3,)
@@ -62,12 +71,12 @@ Base.IndexStyle(::Type{<:Union{Miller,MillerBravais}}) = IndexLinear()
 
 Base.getindex(x::Union{Miller,MillerBravais}, i::Integer) = getindex(x.data, i)
 
-Base.convert(::Type{T}, x::T) where {T<:INDICES} = x
-Base.convert(::Type{Miller{T}}, mb::MillerBravais{T}) where {T<:RealSpace} =
-    Miller{T}(2 * mb[1] + mb[2], 2 * mb[2] + mb[1], mb[4])
-Base.convert(::Type{Miller{T}}, mb::MillerBravais{T}) where {T<:ReciprocalSpace} =
-    Miller{T}(mb[1], mb[2], mb[4])
-Base.convert(::Type{MillerBravais{T}}, m::Miller{T}) where {T<:RealSpace} =
-    MillerBravais{T}(2 * m[1] - m[2], 2 * m[2] - m[1], -(m[1] + m[2]), 3 * m[3])
-Base.convert(::Type{MillerBravais{T}}, m::Miller{T}) where {T<:ReciprocalSpace} =
-    MillerBravais{T}(m[1], m[2], -(m[1] + m[2]), m[3])
+Base.convert(::Type{T}, x::T) where {T<:Union{Miller,MillerBravais}} = x
+Base.convert(::Type{Miller}, mb::MillerBravais) =
+    Miller(2 * mb[1] + mb[2], 2 * mb[2] + mb[1], mb[4])
+Base.convert(::Type{ReciprocalMiller}, mb::ReciprocalMillerBravais) =
+    ReciprocalMiller(mb[1], mb[2], mb[4])
+Base.convert(::Type{MillerBravais}, m::Miller) =
+    MillerBravais(2 * m[1] - m[2], 2 * m[2] - m[1], -(m[1] + m[2]), 3 * m[3])
+Base.convert(::Type{ReciprocalMillerBravais}, m::ReciprocalMiller) =
+    ReciprocalMillerBravais(m[1], m[2], -(m[1] + m[2]), m[3])

commit 458f4fd4e73309a1b7b1ded2fc22f0c1030162c8

Fix a bug in `_indices_str`
introduced in commit
8361e72bde8b237c269c0a3cc2dbed868adc13af

src/miller.jl
@@ -42,14 +42,18 @@ function _indices_str(r::Regex, s::AbstractString)
     else
         brackets = first(m.captures) * last(m.captures)
         x = (parse(Int, x) for x in m.captures[2:(end-1)])
-        if brackets == "()"
-            ReciprocalMiller(x...)
-        elseif brackets == "{}"
-            ReciprocalMillerBravais(x...)
-        elseif brackets == "[]"
-            Miller(x...)
-        elseif brackets == "<>"
-            MillerBravais(x...)
+        if brackets in ("()", "{}")
+            if length(x) == 3
+                ReciprocalMiller(x...)
+            else  # length(x) == 4
+                ReciprocalMillerBravais(x...)
+            end
+        elseif brackets ∈ ("[]", "<>")
+            if length(x) == 3
+                Miller(x...)
+            else  # length(x) == 4
+                MillerBravais(x...)
+            end
         else
             @assert false "this should never happen!"
         end