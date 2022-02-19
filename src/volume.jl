8c94634ec632cc8e1e7cb76c24f19a97d3605601
singularitti <singularitti@outlook.com>
2022-02-19 12:22:06 -0500

    Add docs to `crystaldensity`

--- src/volume.jl
+++ src/volume.jl
@@ -5,7 +5,7 @@ export cellvolume, crystaldensity
 """
     cellvolume(a, b, c, α, β, γ)
 
-Calculates the cell volume from 6 cell parameters.
+Calculate the cell volume from 6 cell parameters.
 """
 cellvolume(a, b, c, α, β, γ) =
     a * b * c * sqrt(sind(α)^2 - cosd(β)^2 - cosd(γ)^2 + 2 * cosd(α) * cosd(β) * cosd(γ))
@@ -13,17 +13,27 @@ cellvolume(a, b, c, α, β, γ) =
     cellvolume(l::Lattice)
     cellvolume(c::Cell)
 
-Calculates the cell volume from a `Lattice` or a `Cell`.
+Calculate the cell volume from a `Lattice` or a `Cell`.
 """
 cellvolume(lattice::AbstractLattice) = abs(det(lattice.data))
 cellvolume(cell::Cell) = abs(det(cell.lattice))
 """
     cellvolume(g::MetricTensor)
 
-Calculates the cell volume from a `MetricTensor`.
+Calculate the cell volume from a `MetricTensor`.
 """
 cellvolume(g::MetricTensor) = sqrt(det(g.data))  # `sqrt` is always positive!
 
+"""
+    crystaldensity(volume::Number, mass::Number)
+    crystaldensity(lattice::Lattice, atoms)
+    crystaldensity(cell::Cell)
+
+Calculate the density of a crystal structure.
+
+Here, `atoms` is an iterable of atomic numbers, element names, symbols, or `PeriodicTable.Element`s.
+You can extend the `atomicmass` method to work with custom types.
+"""
 crystaldensity(volume::Number, mass::Number) = mass / volume
 function crystaldensity(lattice::Lattice, atoms)
     mass = sum(atomicmass, atoms)
