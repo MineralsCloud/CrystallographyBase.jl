34fb20823c8a426ff1c36cff7496f583f6866cea
singularitti <singularitti@outlook.com>
2023-01-13 00:02:58 -0500

    Fix test "Test `supercell` from hexagonal to orthorhombic for a `Lattice`"
    wrong in #73

--- test/supercell.jl
+++ test/supercell.jl
@@ -19,15 +19,25 @@ end
     @test reciprocal(a) == reciprocal(b)
 end
 
-@testset "Test `supercell` from orthogonal to hexagonal for a `Lattice`" begin
-    lattice = Lattice(2, 1, 5, 90, 90, 90; axis=:c)
+# Example from http://staff.ustc.edu.cn/~zqj/posts/Transforming-between-supercell-and-primitive-cell/
+@testset "Test `supercell` from hexagonal to orthorhombic for a `Lattice`" begin
+    lattice = Lattice(2, 2, 3, 90, 90, 120; axis=:c)  # Primitive is hexagonal
     P = [
-        1 1 0
+        2 1 0  # 𝐨₁ = 2𝐡₁ + 𝐡₂
+        0 1 0  # 𝐨₂ = 𝐡₂
+        0 0 1  # 𝐨₃ = 𝐡₃
+    ]'  # Note the transpose!
+    a = supercell(lattice, P)  # Hexagonal to orthorhombic
+    b = Lattice([
+        2√3 0 0
         0 2 0
-        0 0 1
-    ]
-    a = supercell(lattice, P)
-    b = StandardizedFromPrimitive(P)(a)
-    @test a == b
-    @test reciprocal(a) == reciprocal(b)
+        0 0 3
+    ])  # The orthorhombic supercell
+    @test a ≈ b
+    @test reciprocal(a) ≈ reciprocal(b)
+    transform = StandardizedFromPrimitive(P)
+    c = transform(lattice)
+    @test a ≈ c
+    @test reciprocal(a) ≈ reciprocal(c)
+    @test inv(transform)(c) ≈ lattice
 end
