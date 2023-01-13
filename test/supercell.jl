74ca4351693128c2b5732fcd0fc70a0458639d5a
singularitti <singularitti@outlook.com>
2023-01-13 02:45:31 -0500

    Remove "Test `supercell` from primitive to body-centered for a `Lattice`"

--- test/supercell.jl
+++ test/supercell.jl
@@ -6,19 +6,6 @@
     @test reciprocal(a) == reciprocal(b)
 end
 
-@testset "Test `supercell` from primitive to body-centered for a `Lattice`" begin
-    lattice = Lattice(2, 1, 5, 90, 90, 90; axis=:c)
-    P = [
-        0 1 1
-        1 0 1
-        1 1 0
-    ]
-    a = supercell(lattice, P)
-    b = StandardizedFromPrimitive(P)(a)
-    @test a == b
-    @test reciprocal(a) == reciprocal(b)
-end
-
 # Example from http://staff.ustc.edu.cn/~zqj/posts/Transforming-between-supercell-and-primitive-cell/
 @testset "Test `supercell` from hexagonal to orthorhombic for a `Lattice`" begin
     lattice = Lattice(2, 2, 3, 90, 90, 120; axis=:c)  # Primitive is hexagonal
