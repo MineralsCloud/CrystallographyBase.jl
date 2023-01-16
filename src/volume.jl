ceb4c3b5890033bc8506dbb3d950809b92b2d637
singularitti <singularitti@outlook.com>
2023-01-16 04:11:22 -0500

    Move `crystaldensity` & `atomicmass` to src/crystaldensity.jl

--- src/volume.jl
+++ src/volume.jl
@@ -1,6 +1,4 @@
-using PeriodicTable: Element, elements
-
-export cellvolume, crystaldensity
+export cellvolume
 
 """
     cellvolume(a, b, c, α, β, γ)
@@ -23,23 +21,3 @@ cellvolume(cell::Cell) = cellvolume(Lattice(cell))
 Calculate the cell volume from a `MetricTensor`.
 """
 cellvolume(g::MetricTensor) = sqrt(det(g.data))  # `sqrt` is always positive!
-
-"""
-    crystaldensity(lattice::Lattice, atoms)
-    crystaldensity(cell::Cell)
-
-Calculate the density of a crystal structure.
-
-Here, `atoms` is an iterable of atomic numbers, element names, symbols, or `PeriodicTable.Element`s.
-You can extend the `atomicmass` method to work with custom types.
-"""
-function crystaldensity(lattice::Lattice, atoms)
-    mass = sum(atomicmass, atoms)
-    volume = cellvolume(lattice)
-    return mass / volume
-end
-crystaldensity(cell::Cell) = crystaldensity(Lattice(cell), cell.atoms)
-const density = crystaldensity  # For compatibility reason
-
-atomicmass(element::Element) = element.atomic_mass
-atomicmass(i::Union{AbstractString,Integer,Symbol}) = elements[i].atomic_mass
