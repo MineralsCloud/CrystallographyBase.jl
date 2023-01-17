1ad2d98a364931c98cba477b16058a08b7f28b7b
singularitti <singularitti@outlook.com>
2023-01-17 03:28:32 -0500

    Remove tests of `directioncosine` in test/metric.jl

--- test/metric.jl
+++ test/metric.jl
@@ -21,24 +21,8 @@ end
     @test distance(a, g, b)^2 == 13 / 9
 end
 
-@testset "Test direction cosine in a tetragonal lattice" begin
-    g = MetricTensor(2, 2, 3, 90, 90, 90)  # Primitive tetragonal
-    @test g == MetricTensor(Lattice(diagm([2, 2, 3])))
-    @test g == MetricTensor([2, 0, 0], [0, 2, 0], [0, 0, 3])
-    a = [1, 2, 1]
-    b = [0, 0, 1]
-    @test directioncosine(a, g, b)^2 ≈ 9 / 29
-end
-
 # @testset "Symbolic calculation" begin
 #     a, c = symbols("a, c", positive = true)
 #     @test MetricTensor(a, a, c, 90, 90, 120) ==
 #           MetricTensor([a^2 -0.5*a^2 0; -0.5*a^2 a^2 0; 0 0 c^2])  # Primitive hexagonal
 # end
-
-# From https://ssd.phys.strath.ac.uk/wp-content/uploads/Crystallographic_maths.pdf
-@testset "Find angle θ between [100] and [111] directions in GaN" begin
-    a, c = 3.19u"angstrom", 5.19u"angstrom"
-    g = MetricTensor(a, a, c, 90, 90, 120)
-    @test directioncosine([1, 0, 0], g, [1, 1, 1]) == a / 2 / sqrt(a^2 + c^2)
-end
