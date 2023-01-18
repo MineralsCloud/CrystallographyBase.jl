7d3560c0604e7648f715eeec692df71ae314d8f9
singularitti <singularitti@outlook.com>
2023-01-18 03:07:24 -0500

    Deprecate src/crystaldensity.jl

--- src/CrystallographyBase.jl
+++ src/CrystallographyBase.jl
@@ -2,20 +2,8 @@ module CrystallographyBase
 
 using Functors: @functor
 using LinearAlgebra: dot
-using Requires: @require
 using StaticArrays: SVector, SMatrix
 using StructEquality: @struct_hash_equal_isequal_isapprox
-
-function __init__()
-    @require PeriodicTable = "7b2266bf-644c-5ea3-82d8-af4bbd25a884" begin
-        @eval using PeriodicTable: Element, elements
-        include("crystaldensity.jl")
-    end
-    @require Mendeleev = "c116f080-063d-490a-9873-2b5b2cce4c34" begin
-        @eval using Mendeleev: Element, elements
-        include("crystaldensity.jl")
-    end
-end
 
 # `LinearAlgebra.det` is much slower and more inaccurate than my simple `_det`.
 function _det(matrix)
