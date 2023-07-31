561325b93e54e33c35b88f017dde5d721a93569d
singularitti <singularitti@outlook.com>
2023-07-31 14:40:55 -0400

    Rewrite `show` methods

--- src/show.jl
+++ src/show.jl
@@ -1,47 +1,50 @@
-function Base.show(io::IO, x::AbstractLattice)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(x)
-        Base.show_default(IOContext(io, :limit => true), x)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
+function Base.show(io::IO, lattice::AbstractLattice)
+    if get(io, :compact, false)
+        print(io, typeof(lattice), latticeconstants(lattice))
     else
-        println(io, string(typeof(x)))
-        for row in eachrow(x.data)
-            println(io, ' ', join(row, "  "))
-        end
-        println(io, " (a, b, c, α, β, γ) = ", latticeconstants(x))
+        print(io, typeof(lattice), '(')
+        join(
+            io,
+            (
+                "$x=$y" for
+                (x, y) in zip(('a', 'b', 'c', 'α', 'β', 'γ'), latticeconstants(lattice))
+            ),
+            ", ",
+        )
+        print(io, ')')
     end
 end
-function Base.show(io::IO, cell::Cell)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(cell)
-        Base.show_default(IOContext(io, :limit => true), cell)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
-    else
-        println(io, string(typeof(cell)))
-        println(io, " lattice:")
-        for row in eachrow(cell.lattice.data)
-            println(io, "  ", join(row, "  "))
-        end
-        N = natoms(cell)
-        println(io, " $N atomic positions:")
-        for pos in cell.positions
-            println(io, "  ", pos)
-        end
-        println(io, " $N atoms:")
-        println(io, "  ", cell.atoms)
-    end
+function Base.show(io::IO, ::MIME"text/plain", lattice::AbstractLattice)
+    summary(io, lattice)
+    println(io)
+    join(io, ' ' * join(row, "  ") * '\n' for row in eachrow(lattice.data))
+    return nothing
 end
-function Base.show(io::IO, x::ReciprocalPoint)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(x)
-        Base.show_default(IOContext(io, :limit => true), x)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
-    else
-        println(io, string(typeof(x)))
-        print(io, " coord = ", x.coord, ", weight = ", x.weight)
+function Base.show(io::IO, ::MIME"text/plain", cell::Cell)
+    summary(io, cell)
+    println(io)
+    println(io, " lattice:")
+    for row in eachrow(cell.lattice.data)
+        println(io, "  ", join(row, "  "))
     end
+    N = natoms(cell)
+    println(io, " $N atomic positions:")
+    for pos in cell.positions
+        println(io, "  ", pos)
+    end
+    println(io, " $N atoms:")
+    println(io, "  ", cell.atoms)
+    return nothing
 end
-function Base.show(io::IO, x::ChangeOfBasis)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(x)
-        Base.show_default(IOContext(io, :limit => true), x)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
-    else
-        println(io, string(typeof(x)))
-        for row in eachrow(x.tf)
-            println(io, ' ', join(row, "  "))
-        end
+function Base.show(io::IO, ::MIME"text/plain", point::ReciprocalPoint)
+    println(io, string(typeof(point)))
+    println(io, " coord=", point.coord)
+    print(io, " weight=", point.weight)
+    return nothing
+end
+function Base.show(io::IO, ::MIME"text/plain", A::ChangeOfBasis)
+    println(io, string(typeof(A)))
+    for row in eachrow(A.tf)
+        println(io, ' ', join(row, "  "))
     end
 end
