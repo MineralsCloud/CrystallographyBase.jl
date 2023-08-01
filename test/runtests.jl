7c3d09bd2a3751ba3aeb5f4b40dc589d05d5b246
singularitti <singularitti@outlook.com>
2023-07-31 22:25:27 -0400

    Add test/systems.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Unitful: @u_str
 using UnitfulAtomic
 
 @testset "CrystallographyBase.jl" begin
+    include("systems.jl")
     include("lattice.jl")
     include("supercell.jl")
     # include("volume.jl")
---
+++ test/systems.jl
@@ -0,0 +1,29 @@
+@testset "Test construction from symbols and numbers" begin
+    @test LatticeSystem(1) == LatticeSystem(:Triclinic)
+    @test LatticeSystem(:Monoclinic) == LatticeSystem(2)
+    @test LatticeSystem(:Rhombohedral) == LatticeSystem(6)
+    @test CrystalSystem(:Triclinic) == CrystalSystem(1)
+    @test CrystalSystem(4) == CrystalSystem(:Tetragonal)
+    @test CrystalSystem(:Trigonal) == CrystalSystem(5)
+    @test Bravais(:PrimitiveTetragonal) == Bravais(8)
+    @test Bravais(:BodyCenteredTetragonal) == Bravais(9)
+    @test Bravais(14) == Bravais(:FaceCenteredCubic)
+end
+
+@testset "Test instances of different types are different" begin
+    @test LatticeSystem(4) != CrystalSystem(3)
+    @test LatticeSystem(:Tetragonal) != Bravais(:PrimitiveTetragonal)
+    @test CrystalSystem(:Monoclinic) != Bravais(:BaseCenteredMonoclinic)
+end
+
+@testset "Test instances of different types are different even if they have the same name" begin
+    @test LatticeSystem(:Orthorhombic) != CrystalSystem(:Orthorhombic)
+    @test LatticeSystem(:Cubic) != CrystalSystem(:Cubic)
+    @test LatticeSystem(4) != CrystalSystem(4)
+end
+
+@testset "Test `instances`" begin
+    @test instances(LatticeSystem) == 7
+    @test instances(CrystalSystem) == 7
+    @test instances(Bravais) == 14
+end
