b9f246b1f348eb6d61b4d2bf77946ea450749130
singularitti <singularitti@outlook.com>
2023-08-18 00:50:31 -0400

    Deprecate `show` for `Lattice`

--- src/show.jl
+++ src/show.jl
@@ -1,19 +1,3 @@
-function Base.show(io::IO, lattice::Lattice)
-    if get(io, :compact, false)
-        print(io, typeof(lattice), latticeconstants(lattice))
-    else
-        print(io, typeof(lattice), '(')
-        join(
-            io,
-            (
-                "$x=$y" for
-                (x, y) in zip(('a', 'b', 'c', 'α', 'β', 'γ'), latticeconstants(lattice))
-            ),
-            ", ",
-        )
-        print(io, ')')
-    end
-end
 function Base.show(io::IO, ::MIME"text/plain", point::ReciprocalPoint)
     println(io, string(typeof(point)))
     println(io, " coord=", point.coord)
