693ee4731b1d905f7cf3019a4bbd20133abbb2ec
singularitti <singularitti@outlook.com>
2023-09-14 04:55:34 -0400

    Deprecate `CartesianFromReduced` & `ReducedFromCartesian`, just use matrix operations

--- src/transform.jl
+++ src/transform.jl
@@ -1,74 +1,7 @@
-export ReducedFromCartesian,
-    CartesianFromReduced,
-    ReducedToCartesian,
-    CartesianToReduced,
-    StandardizedFromPrimitive,
-    PrimitiveFromStandardized,
-    PrimitiveToStandardized,
-    StandardizedToPrimitive
+export StandardizedFromPrimitive,
+    PrimitiveFromStandardized, PrimitiveToStandardized, StandardizedToPrimitive
 
 abstract type ChangeOfBasis{T} <: AbstractMatrix{T} end
-struct CartesianFromReduced{T} <: ChangeOfBasis{T}
-    tf::MMatrix{3,3,T,9}
-end
-struct ReducedFromCartesian{T} <: ChangeOfBasis{T}
-    tf::MMatrix{3,3,T,9}
-end
-# This requires the a-vector is parallel to the Cartesian x-axis.
-# See https://en.wikipedia.org/wiki/Fractional_coordinates
-"""
-    CartesianFromReduced(lattice::Union{Lattice,ReciprocalLattice})
-    CartesianFromReduced(a, b, c, α, β, γ)
-
-Get the transformation from fractional coordinates to Cartesian coordinates.
-"""
-CartesianFromReduced(lattice::Lattice) = CartesianFromReduced(parent(lattice))
-CartesianFromReduced(lattice::ReciprocalLattice) =
-    CartesianFromReduced(transpose(parent(lattice)))
-function CartesianFromReduced(a, b, c, α, β, γ)
-    Ω = cellvolume(a, b, c, α, β, γ)
-    b_sinγ, b_cosγ = b .* (sind(γ), cosd(γ))
-    return CartesianFromReduced(
-        [
-            a b_cosγ c*cosd(β)
-            0 b_sinγ c*_auxiliary(α, β, γ)
-            0 0 Ω/(a * b_sinγ)
-        ]
-    )
-end
-"""
-    ReducedFromCartesian(lattice::Union{Lattice,ReciprocalLattice})
-    ReducedFromCartesian(a, b, c, α, β, γ)
-
-Get the transformation from Cartesian coordinates to fractional coordinates.
-"""
-ReducedFromCartesian(lattice::Lattice) = ReducedFromCartesian(inv(parent(lattice)))
-ReducedFromCartesian(lattice::ReciprocalLattice) =
-    ReducedFromCartesian(transpose(inv(parent(lattice))))
-function ReducedFromCartesian(a, b, c, α, β, γ)
-    Ω = cellvolume(a, b, c, α, β, γ)
-    b_sinγ = b * sind(γ)
-    return ReducedFromCartesian(
-        [
-            1/a -cotd(γ)/a -b * c * _auxiliary(β, α, γ)/Ω
-            0 1/b_sinγ -a * c * _auxiliary(α, β, γ)/Ω
-            0 0 a * b_sinγ/Ω
-        ],
-    )
-end
-const ReducedToCartesian = CartesianFromReduced
-const CartesianToReduced = ReducedFromCartesian
-
-# This is a helper function and should not be exported!
-_auxiliary(α, β, γ) = (cosd(α) - cosd(β) * cosd(γ)) / sind(γ)
-
-(x::Union{CartesianFromReduced,ReducedFromCartesian})(v) = x.tf * collect(v)
-
-Base.inv(x::ReducedFromCartesian) = CartesianFromReduced(inv(x.tf))
-Base.inv(x::CartesianFromReduced) = ReducedFromCartesian(inv(x.tf))
-Base.:∘(x::CartesianFromReduced, y::ReducedFromCartesian) = ∘(y, x)
-Base.:∘(x::ReducedFromCartesian, y::CartesianFromReduced) =
-    x.tf * y.tf ≈ I ? identity : error("undefined!")
 
 # Idea from https://spglib.github.io/spglib/definition.html#transformation-to-the-primitive-cell
 struct StandardizedFromPrimitive{T} <: ChangeOfBasis{T}
