aadbafdacd62b018076baa531c66f5bb60ea0cd3
singularitti <singularitti@outlook.com>
2023-09-18 01:24:48 -0400

    Implement `eachatomgroup` in src/eachatomgroup.jl

--- src/eachatomgroup.jl
+++ src/eachatomgroup.jl
@@ -1,4 +1,13 @@
+export eachatomgroup
+
 struct AtomGroup{A,B}
     atom::A
     positions::Vector{B}
+end
+function eachatomgroup(cell::Cell)
+    types = atomtypes(cell)
+    return Iterators.map(types) do type
+        indices = findall(==(type), cell.atoms)
+        AtomGroup(type, cell.positions[indices])
+    end
 end
