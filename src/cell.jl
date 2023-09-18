57200fff689c502caa25b651b190b9e6d3db0685
singularitti <singularitti@outlook.com>
2023-09-18 05:14:10 -0400

    Fix `super` for `Cell`
    See https://github.com/MineralsCloud/CrystallographyBase.jl/issues/111

--- src/cell.jl
+++ src/cell.jl
@@ -16,18 +16,17 @@ function super(cell::Cell, factors::AbstractMatrix{<:Integer})
         throw(ArgumentError("`factors` must be a 3×3 matrix!"))
     end
     @assert isdiag(factors) "currently not supported!"
-    @assert _det(factors) >= 1
-    new_atoms = eltype(cell.atoms)[]
-    new_positions = eltype(cell.positions)[]
+    @assert all(factor >= 1 for factor in diag(factors)) "all factors must be greater than or equal to one!"
     l, m, n = diag(factors)
     𝐚, 𝐛, 𝐜 = eachcol(Matrix(I, 3, 3))
+    new_atoms = eltype(cell.atoms)[]
+    new_positions = eltype(cell.positions)[]
     for (atom, position) in eachatom(cell)
         for (i, j, k) in Iterators.product(0:(l - 1), 0:(m - 1), 0:(n - 1))
-            push!(new_atoms, atom)
-            # See https://doi.org/10.1186/s13321-016-0129-3
-            new_position = position + i * 𝐚 + j * 𝐛 + k * 𝐜
-            new_position ./= (l, m, n)  # Make them within the boundary of the cell
+            # See https://doi.org/10.1186/s13321-016-0129-3 and #111
+            new_position = (position + i * 𝐚 + j * 𝐛 + k * 𝐜) ./ (l, m, n)  # Make them within the boundary of the cell
             push!(new_positions, new_position)
+            push!(new_atoms, atom)
         end
     end
     new_lattice = super(cell.lattice, factors)
