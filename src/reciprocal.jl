19f44deaf8b233673f1d280ab6db4020f4284250
singularitti <singularitti@outlook.com>
2023-09-22 23:05:51 -0400

    Add `BrillouinZone` type

--- src/reciprocal.jl
+++ src/reciprocal.jl
@@ -20,3 +20,20 @@ struct MonkhorstPackGrid
         return new(mesh, is_shift)
     end
 end
+
+@anonymousenum BrillouinZone::UInt8 begin
+    # PrimitiveTriclinic = 1
+    # PrimitiveMonoclinic = 2
+    # BaseCenteredMonoclinic = 3
+    # PrimitiveOrthorhombic = 4
+    # BaseCenteredOrthorhombic = 5
+    # BodyCenteredOrthorhombic = 6
+    # FaceCenteredOrthorhombic = 7
+    # PrimitiveTetragonal = 8
+    # BodyCenteredTetragonal = 9
+    PrimitiveHexagonal = 10
+    # PrimitiveRhombohedral = 11
+    PrimitiveCubic = 12
+    BodyCenteredCubic = 13
+    FaceCenteredCubic = 14
+end
