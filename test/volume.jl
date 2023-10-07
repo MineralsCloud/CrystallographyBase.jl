4042d7d1fcbdda5bc2a1b90e58f3a074f14fccd7
singularitti <singularitti@outlook.com>
2023-10-07 03:44:15 -0400

    Fix data in "Test `crystaldensity`"

--- test/volume.jl
+++ test/volume.jl
@@ -4,37 +4,37 @@ using Unitful: uconvert
 # Data from https://materialsproject.org/materials/mp-2657/
 @testset "Test `crystaldensity`" begin
     lattice = Lattice(
-        4.653272u"angstrom", 4.653272u"angstrom", 2.969203u"angstrom", 90, 90, 90
+        4.59983732u"angstrom", 4.59983732u"angstrom", 2.95921356u"angstrom", 90, 90, 90
     )
     positions = [
-        [0, 0, 0],
-        [0.5, 0.5, 0.5],
-        [0.19542, 0.80458, 0.5],
-        [0.80458, 0.19542, 0.5],
-        [0.30458, 0.30458, 0],
-        [0.69542, 0.69542, 0],
+        [0, 0, 0.5],
+        [0.5, 0.5, 0.0],
+        [0.19567869, 0.80432131, 0],
+        [0.80432131, 0.19567869, 0],
+        [0.30432131, 0.30432131, 0.5],
+        [0.69567869, 0.69567869, 0.5],
     ]
     @testset "Test `Element`s" begin
         atoms = elements[[22, 22, 8, 8, 8, 8]]
         cell = Cell(lattice, positions, atoms)
-        @test cellvolume(cell) ≈ 64.29197531534862u"angstrom^3"
+        @test cellvolume(cell) ≈ 62.612530083185085u"angstrom^3"
         @test uconvert(u"g/cm^3", crystaldensity(lattice, atoms)) ≈
-              4.125526333805304u"g/cm^3"
-        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.125526333805304u"g/cm^3"
+            4.236179319948116u"g/cm^3"
+        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.236179319948116u"g/cm^3"
     end
     @testset "Test `AbstractString`s" begin
         atoms = elements[["Titanium", "Titanium", "Oxygen", "Oxygen", "Oxygen", "Oxygen"]]
         cell = Cell(lattice, positions, atoms)
-        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.125526333805304u"g/cm^3"
+        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.236179319948116u"g/cm^3"
     end
     @testset "Test `Integer`s" begin
         atoms = [22, 22, 8, 8, 8, 8]
         cell = Cell(lattice, positions, atoms)
-        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.125526333805304u"g/cm^3"
+        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.236179319948116u"g/cm^3"
     end
     @testset "Test `Symbol`s" begin
         atoms = [:Ti, :Ti, :O, :O, :O, :O]
         cell = Cell(lattice, positions, atoms)
-        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.125526333805304u"g/cm^3"
+        @test uconvert(u"g/cm^3", crystaldensity(cell)) ≈ 4.236179319948116u"g/cm^3"
     end
 end
