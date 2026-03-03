0ac756b6932604f889bf33e8593c8a2ed9ce250b
singularitti <singularitti@outlook.com>
2026-03-03 04:47:08 -0600

    Simplify `unwrap_convert`

--- ext/SpglibExt.jl
+++ ext/SpglibExt.jl
@@ -5,21 +5,8 @@ using CrystallographyBase: MagneticAtom
 import CrystallographyCore: Cell
 import Spglib: SpglibCell, unwrap_convert
 
-function unwrap_convert(cell::Cell{L,P,<:MagneticAtom}) where {L,P}
-    lattice, positions = cell.lattice, cell.positions
-    clattice = Base.cconvert(Matrix{Cdouble}, permutedims(lattice))
-    cpositions = Base.cconvert(Matrix{Cdouble}, reduce(hcat, positions))
-    atoms = [atom.label for atom in cell.atoms]
-    magmoms = [atom.magnetic_moment for atom in cell.atoms]
-    atomtypes = unique(atoms)
-    catoms = collect(Cint, findfirst(==(atom), atomtypes) for atom in atoms)  # Mapping between unique atom types and atom indices
-    cmagmoms = if eltype(magmoms) <: AbstractVector
-        Base.cconvert(Matrix{Cdouble}, reduce(hcat, magmoms))
-    else
-        Base.cconvert(Vector{Cdouble}, magmoms)  # `magmoms` could be empty!
-    end
-    return clattice, cpositions, catoms, cmagmoms
-end
+unwrap_convert(cell::Cell{L,P,<:MagneticAtom}) where {L,P} =
+    unwrap_convert(SpglibCell(cell))
 
 function SpglibCell(cell::Cell{L,P,<:MagneticAtom}) where {L,P}
     atoms = [atom.label for atom in cell.atoms]
