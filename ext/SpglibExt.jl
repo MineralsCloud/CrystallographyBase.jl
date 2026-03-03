c787d15e841a997c69a7abe682693dc7e888eb7a
singularitti <singularitti@outlook.com>
2026-03-03 04:44:05 -0600

    Add `SpglibCell` from `Cell{L,P,<:MagneticAtom}` and vice versa

--- ext/SpglibExt.jl
+++ ext/SpglibExt.jl
@@ -1,8 +1,9 @@
 module SpglibExt
 
-using CrystallographyBase: Cell, MagneticAtom
+using CrystallographyBase: MagneticAtom
 
-import Spglib: unwrap_convert
+import CrystallographyCore: Cell
+import Spglib: SpglibCell, unwrap_convert
 
 function unwrap_convert(cell::Cell{L,P,<:MagneticAtom}) where {L,P}
     lattice, positions = cell.lattice, cell.positions
@@ -18,6 +19,21 @@ function unwrap_convert(cell::Cell{L,P,<:MagneticAtom}) where {L,P}
         Base.cconvert(Vector{Cdouble}, magmoms)  # `magmoms` could be empty!
     end
     return clattice, cpositions, catoms, cmagmoms
+end
+
+function SpglibCell(cell::Cell{L,P,<:MagneticAtom}) where {L,P}
+    atoms = [atom.label for atom in cell.atoms]
+    magmoms = [atom.magnetic_moment for atom in cell.atoms]
+    return SpglibCell(cell.lattice, cell.positions, atoms, magmoms)
+end
+
+function Cell(cell::SpglibCell)
+    if isempty(cell.magmoms)
+        return Cell(cell.lattice, cell.positions, cell.atoms)  # Non-magnetic case
+    else
+        atoms = MagneticAtom.(cell.atoms, cell.magmoms)  # Magnetic case
+        return Cell(cell.lattice, cell.positions, atoms)
+    end
 end
 
 end
