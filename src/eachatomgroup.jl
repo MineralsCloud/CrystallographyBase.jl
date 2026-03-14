47d3801959381886a638292091ba47eae6984c8c
singularitti <singularitti@outlook.com>
2026-03-13 20:51:56 -0500

    Add docs to `eachatomgroup`

--- src/eachatomgroup.jl
+++ src/eachatomgroup.jl
@@ -30,6 +30,32 @@ function eachatom(group::AtomGroup, cell::AbstractCell)
     )
 end
 
+"""
+    eachatomgroup(cell::AbstractCell)
+
+Iterate over atom groups in `cell`, grouped by atom value/type.
+
+For each distinct value in `cell.atoms`, this function returns an
+[`AtomGroup`](@ref) with:
+- `atom`: that distinct atom value,
+- `indices`: indices into `cell.atoms` / `cell.positions` of all atoms of that type.
+
+The output order follows `atomtypes(cell)` (first appearance order in
+`cell.atoms`).
+
+# Examples
+```jldoctest
+julia> cell = Cell(rand(3, 3), [[0, 0, 0], [0.5, 0.5, 0.5], [0.25, 0.25, 0.25]], [:Si, :Si, :O])
+
+julia> group = collect(eachatomgroup(cell))[1]
+AtomGroup{2, Symbol}(:Si, (1, 2))
+
+julia> cell.positions[collect(group.indices)]
+2-element Array{ReducedCoordinates{Float64}, 1}:
+ [0.0, 0.0, 0.0]
+ [0.5, 0.5, 0.5]
+```
+"""
 function eachatomgroup(cell::AbstractCell)
     types = atomtypes(cell)
     return Iterators.map(types) do type
